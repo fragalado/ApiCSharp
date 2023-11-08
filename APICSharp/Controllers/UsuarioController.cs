cb20bb3b2ecda0cef46fd81640ecda03dfbd17aa
DESKTOP-V1IS21O\Puesto8 <frandanigd@gmail.com>
2023-11-08 14:42:34 +0100

    Agregar archivos de proyecto.

---
+++ APICSharp/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public UsuarioController(Contexto contexto)
+        {
+            _context = contexto;
+        }
+
+        // Select
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> GetUsuarios()
+        {
+            return _context.Usuarios.ToList();
+        }
+
+        // Insert
+        [HttpPost]
+        public ActionResult<Usuario> PostUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+
+            return CreatedAtAction(nameof(GetUsuarios), new { id = usuario.id_usuario }, usuario);
+        }
+
+        // Update
+        [HttpPut]
+        public IActionResult UpdateUsuario(Usuario usuario)
+        {
+            _context.Update(usuario);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // Delete
+    }
+}
